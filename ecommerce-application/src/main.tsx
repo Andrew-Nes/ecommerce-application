4eb120132983fee8eacd480e1ebb9ed747c0d575
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-08-10 21:20:56 +0400

    refactor: remove LoginForm from main.tsx

--- ecommerce-application/src/main.tsx
+++ ecommerce-application/src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import LoginForm from './components/forms/loginForm/loginForm';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LoginForm />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 );
