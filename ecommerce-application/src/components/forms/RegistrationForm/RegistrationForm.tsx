d6d09005305efdacae4956496923d50bbdc11596
Mikita <anykey2solo@gmail.com>
2023-08-13 16:06:05 +0300

    refactor: apply eslint to RegistrForm

--- ecommerce-application/src/components/forms/RegistrationForm/RegistrationForm.tsx
+++ ecommerce-application/src/components/forms/RegistrationForm/RegistrationForm.tsx
@@ -1,92 +1,85 @@
 import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import type { RegistrationFormData } from '../../../types/registrationFormTypes';
-import MyInput from './inputs/MyInput'
+import MyInput from './inputs/MyInput';
 import classes from './RegistrationForm.module.scss';
 import { validateFields } from './validateFields';
 import MyCountrySelect from './inputs/MyCountrySelect';
 import MyCheckBox from './inputs/myCheckBox/MyCheckBox';
 
-
-
-
 export default function RegistrationForm() {
-    const {
-      register,
-      handleSubmit,
-      reset,
-      formState: { errors, isValid },
-      getValues,
-      setValue,
-    } = useForm<RegistrationFormData>({ mode: 'all' });
-  
-    const [isChoose, setSameAddress] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+    getValues,
+    setValue,
+  } = useForm<RegistrationFormData>({ mode: 'all' });
 
-    const onSubmit: SubmitHandler<RegistrationFormData> = (data) => {
-        console.log(data);
-        reset();
-      };
+  const [isChoose, setSameAddress] = useState(false);
 
-      function setBillingAddress() {
-        if (!isChoose) {
-          setValue('cityBilling', getValues('cityShipping'));
-          setValue('streetBilling', getValues('streetShipping'));
-          setValue('postalCodeBilling', getValues('postalCodeShipping'));
-          setValue('countryBilling', getValues('countryShipping'));
-          setSameAddress(true);
-          if (isValid) {
-            console.log('lox');
-          }
-        } else {
-          setValue('cityBilling', '');
-          setValue('streetBilling', '');
-          setValue('postalCodeBilling', '');
-          setSameAddress(false);
-        }
-      }
-    
+  const onSubmit: SubmitHandler<RegistrationFormData> = (data) => {
+    console.log(data);
+    reset();
+  };
 
-      return (
-        <div className={classes.registration}>
-        <h3>Registration form:</h3>
-        <form
-          className={classes.register__form}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <fieldset>
-            <legend className={classes.legend_fieldset}>Customer</legend>
-            <MyInput
-              register={register}
-              errors={errors}
-              name="email"
-              title="Email"
-              validate={validateFields.EMAIL_VALIDATE}
-            />
-            <MyInput
-              register={register}
-              errors={errors}
-              name="firstName"
-              title="First Name"
-              validate={validateFields.FIRST_NAME_VALIDATE}
-            />
-            <MyInput
-              register={register}
-              errors={errors}
-              name="lastName"
-              title="Last Name"
-              validate={validateFields.LAST_NAME_VALIDATE}
-            />
-            <MyInput
-              register={register}
-              errors={errors}
-              name="dateOfBirth"
-              title="Date of birth"
-              type="date"
-              validate={validateFields.DATE_OF_BIRTH_VALIDATE}
-            />
-          </fieldset>
+  function setBillingAddress() {
+    if (!isChoose) {
+      setValue('cityBilling', getValues('cityShipping'));
+      setValue('streetBilling', getValues('streetShipping'));
+      setValue('postalCodeBilling', getValues('postalCodeShipping'));
+      setValue('countryBilling', getValues('countryShipping'));
+      setSameAddress(true);
+    } else {
+      setValue('cityBilling', '');
+      setValue('streetBilling', '');
+      setValue('postalCodeBilling', '');
+      setSameAddress(false);
+    }
+  }
 
-          <fieldset>
+  return (
+    <div className={classes.registration}>
+      <h3>Registration form:</h3>
+      <form
+        className={classes.register__form}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <fieldset>
+          <legend className={classes.legend_fieldset}>Customer</legend>
+          <MyInput
+            register={register}
+            errors={errors}
+            name="email"
+            title="Email"
+            validate={validateFields.EMAIL_VALIDATE}
+          />
+          <MyInput
+            register={register}
+            errors={errors}
+            name="firstName"
+            title="First Name"
+            validate={validateFields.FIRST_NAME_VALIDATE}
+          />
+          <MyInput
+            register={register}
+            errors={errors}
+            name="lastName"
+            title="Last Name"
+            validate={validateFields.LAST_NAME_VALIDATE}
+          />
+          <MyInput
+            register={register}
+            errors={errors}
+            name="dateOfBirth"
+            title="Date of birth"
+            type="date"
+            validate={validateFields.DATE_OF_BIRTH_VALIDATE}
+          />
+        </fieldset>
+
+        <fieldset>
           <legend className={classes.legend_fieldset}>Shipping address</legend>
           <MyCheckBox
             register={register}
@@ -167,16 +160,14 @@ export default function RegistrationForm() {
           />
         </fieldset>
 
-
-          <button
+        <button
           className={classes.submit_button}
           disabled={!isValid}
           type="submit"
         >
           register
         </button>
-</form>
-</div>  
-      )
-
+      </form>
+    </div>
+  );
 }
