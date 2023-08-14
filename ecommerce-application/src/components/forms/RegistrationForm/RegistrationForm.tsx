9c2715a9eda85a92d06549fc1e19f418258d547a
Mikita <anykey2solo@gmail.com>
2023-08-14 20:31:04 +0300

    fix: trigger to validate form after copy shipping address to billing

--- ecommerce-application/src/components/forms/RegistrationForm/RegistrationForm.tsx
+++ ecommerce-application/src/components/forms/RegistrationForm/RegistrationForm.tsx
@@ -19,6 +19,7 @@ export default function RegistrationForm() {
     formState: { errors, isValid },
     getValues,
     setValue,
+    trigger
   } = useForm<RegistrationFormData>({ mode: 'all' });
 
   const [isChoose, setSameAddress] = useState(false);
@@ -33,12 +34,15 @@ export default function RegistrationForm() {
       setValue('streetBilling', getValues('streetShipping'));
       setValue('postalCodeBilling', getValues('postalCodeShipping'));
       setValue('countryBilling', getValues('countryShipping'));
+      trigger(['cityBilling','streetBilling', 'postalCodeBilling', 'countryBilling'])
       setSameAddress(true);
+     
     } else {
       setValue('cityBilling', '');
       setValue('streetBilling', '');
       setValue('postalCodeBilling', '');
       setValue('countryBilling', COUNTRIES[defaultCountryIndex]);
+      trigger(['cityBilling','streetBilling', 'postalCodeBilling', 'countryBilling'])
       setSameAddress(false);
     }
   }
