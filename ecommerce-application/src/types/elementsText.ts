6d926917ce081494d1f1587e8672b0f2206cc666
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-08-20 03:20:29 +0400

    feat: add styles for 404 page

--- ecommerce-application/src/types/elementsText.ts
+++ ecommerce-application/src/types/elementsText.ts
@@ -9,6 +9,7 @@ enum anchorsText {
   MAIN = 'Main',
   CREATE_NEW_ACCOUNT = 'Create your account',
   LOGIN = 'Click to login',
+  NOT_FOUND = 'Go back to main',
 }
 
 enum logoText {
@@ -22,12 +23,14 @@ enum headingText {
   CUSTOMER_FORM = 'Customer',
   SHIPPING_ADDRESS_FORM = 'Shipping address',
   BILLING_ADDRESS_FORM = 'Billing address',
+  NOT_FOUND_PAGE_HEADING = 'Oops! Something went wrong',
 }
 
 enum paragraphText {
   MAIN_PAGE_PARAGRAPH = 'If you want to sign up or login just click one of the buttons below:',
   LOGIN_PAGE_PARAGRAPH = 'Not registered? ',
   REGISTRATION_PAGE_PARAGRAPH = 'Already registered? ',
+  NOT_FOUND_PAGE_PARAGRAPH_1 = "We can't find the page you're looking for.",
 }
 
 export { buttonsText, anchorsText, logoText, headingText, paragraphText };
