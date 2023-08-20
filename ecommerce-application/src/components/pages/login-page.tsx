b2580dbebf50009a11ae1e8ef58d2ec5d02d882b
Andrei Niasmachny <Cejosoll@gmail.com>
2023-08-20 17:22:52 +0300

    fix: correct auto redirect for login page

--- ecommerce-application/src/components/pages/login-page.tsx
+++ ecommerce-application/src/components/pages/login-page.tsx
@@ -2,29 +2,32 @@ import './pages.scss';
 import RedirectButton from '../redirect-button/redirect-button';
 import { routes } from '../../types/routingTypes';
 import { loginStateChangeProp } from '../../types/routingTypes';
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { LogInContext } from '../app';
 
 function LoginPage({ loginStateChange }: loginStateChangeProp) {
   const isLoggedIn = useContext(LogInContext);
+  const path = useLocation();
   const redirect = useNavigate();
   function logIn() {
     loginStateChange(true);
   }
-  if (isLoggedIn) {
-    redirect(routes.MAIN);
-  } else {
-    return (
-      <div className="login-page">
-        <h1 onClick={logIn}>This is login page</h1>
-        <div>
-          <p>Don't have an account?</p>
-          <RedirectButton route={routes.REGISTER} text="Click to register" />
-        </div>
+  useEffect(() => {
+    if (isLoggedIn) {
+      redirect(routes.MAIN);
+    }
+  }, [isLoggedIn, path, redirect]);
+
+  return (
+    <div className="login-page">
+      <h1 onClick={logIn}>This is login page</h1>
+      <div>
+        <p>Don't have an account?</p>
+        <RedirectButton route={routes.REGISTER} text="Click to register" />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default LoginPage;
