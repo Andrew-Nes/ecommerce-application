be74b25105b87600b5aa9ffc8e571e6f3251a2ba
Andrei Niasmachny <Cejosoll@gmail.com>
2023-08-20 12:26:53 +0300

    feat: add login context to login page

--- ecommerce-application/src/components/pages/login-page.tsx
+++ ecommerce-application/src/components/pages/login-page.tsx
@@ -1,17 +1,30 @@
 import './pages.scss';
 import RedirectButton from '../redirect-button/redirect-button';
-import { routes } from '../../types/routes';
+import { routes } from '../../types/routingTypes';
+import { loginStateChangeProp } from '../../types/routingTypes';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LogInContext } from '../app';
 
-function LoginPage() {
-  return (
-    <div className="login-page">
-      <h1>This is login page</h1>
-      <div>
-        <p>Don't have an account?</p>
-        <RedirectButton route={routes.REGISTER} text="Click to register" />
+function LoginPage({ loginStateChange }: loginStateChangeProp) {
+  const isLoggedIn = useContext(LogInContext);
+  const redirect = useNavigate();
+  function logIn() {
+    loginStateChange(true);
+  }
+  if (isLoggedIn) {
+    redirect(routes.MAIN);
+  } else {
+    return (
+      <div className="login-page">
+        <h1 onClick={logIn}>This is login page</h1>
+        <div>
+          <p>Don't have an account?</p>
+          <RedirectButton route={routes.REGISTER} text="Click to register" />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default LoginPage;
