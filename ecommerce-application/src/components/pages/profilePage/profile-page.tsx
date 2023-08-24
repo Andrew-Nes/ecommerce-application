b326478ae678ac2978dd11d01ca95da8167547db
Mikita <anykey2solo@gmail.com>
2023-08-24 23:11:29 +0300

    feat: implement redirect unregistered user to login Page

--- ecommerce-application/src/components/pages/profilePage/profile-page.tsx
+++ ecommerce-application/src/components/pages/profilePage/profile-page.tsx
@@ -1,6 +1,18 @@
 import './profile-page.scss';
+import { routes } from '../../../types/routingTypes';
+import { useContext, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { LogInContext } from '../../app';
 
 export default function ProfilePage() {
+  const isLoggedIn = useContext(LogInContext);
+  const path = useLocation();
+  const redirect = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      redirect(routes.LOGIN);
+    }
+  }, [isLoggedIn, path, redirect]);
   return (
     <div>
       <h1>Profile Page</h1>
