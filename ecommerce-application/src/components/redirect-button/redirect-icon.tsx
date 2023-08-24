a5c42c1c3238fdd7b702cc380da97ccc5d5eac4e
Mikita <anykey2solo@gmail.com>
2023-08-24 21:47:15 +0300

    feat: create RedirectIcon component

---
+++ ecommerce-application/src/components/redirect-button/redirect-icon.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { MouseEventHandler } from 'react';
+import { iconProps } from '../../types/routingTypes';
+
+function RedirectIcon({ src, route, className, alt }: iconProps) {
+    const redirect = useNavigate();
+    const redirection: MouseEventHandler = () => {
+      redirect(route);
+    };
+    return (
+      <div className={className} onClick={redirection}>
+        <img src={src} alt={alt} />
+      </div>
+    );
+  }
+  
+  export default RedirectIcon;
