5402abc1ec739d41de77580e1d2a673bcfd838d3
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-08-25 00:14:51 +0400

    feat: add basic styles for catalog page

--- ecommerce-application/src/components/pages/catalogPage/catalog-page.tsx
+++ ecommerce-application/src/components/pages/catalogPage/catalog-page.tsx
@@ -1,3 +1,4 @@
+import './catalog-page.scss';
 import { Link } from 'react-router-dom';
 import { routes } from '../../../types/routingTypes';
 import { anchorsText } from '../../../types/elementsText';
@@ -6,20 +7,25 @@ export default function CatalogPage() {
   return (
     <main className="main catalog-page">
       <div className="wrapper catalog-page__wrapper">
-        <ul className="breadcrumbs">
-          <li>
-            <Link to={routes.MAIN}>{anchorsText.MAIN}</Link>
+        <ul className="breadcrumbs list">
+          <li className="list__item breadcrumbs__item">
+            <Link className="breadcrumbs__list" to={routes.MAIN}>
+              {anchorsText.MAIN}
+            </Link>
           </li>
-          <li>
-            <Link to={routes.CATALOG}>{anchorsText.CATALOG}</Link>
+          <li className="list__item breadcrumbs__item">
+            <Link className="breadcrumbs__list" to={routes.CATALOG}>
+              {anchorsText.CATALOG}
+            </Link>
           </li>
         </ul>
         <div>
-          <h3>Catalog</h3>
+          <h3 className="heading catalog__heading">Catalog</h3>
           <div className="categories">
-            <ul>
-              <li></li>
-            </ul>
+            <div className="category"></div>
+            <div className="category"></div>
+            <div className="category"></div>
+            <div className="category"></div>
           </div>
         </div>
       </div>
