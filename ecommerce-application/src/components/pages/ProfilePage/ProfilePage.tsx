ed71f2bc692ed0624764ab168db5350ec5350406
Mikita <anykey2solo@gmail.com>
2023-08-27 22:10:46 +0300

    feat: add user profile card template

--- ecommerce-application/src/components/pages/ProfilePage/ProfilePage.tsx
+++ ecommerce-application/src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import './profile-page.scss';
 import { routes } from '../../../types/routingTypes';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { LogInContext } from '../../app';
 import { FC } from 'react';
@@ -9,14 +9,39 @@ const ProfilePage: FC = () => {
   const isLoggedIn = useContext(LogInContext);
   const path = useLocation();
   const redirect = useNavigate();
-  useEffect(() => {
-    if (!isLoggedIn) {
-      redirect(routes.LOGIN);
-    }
-  }, [isLoggedIn, path, redirect]);
+  const acessToken = window.localStorage.getItem('token') || ''
+
+  const [modalActive, setModalActive] = useState(false)
+
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dateOfBirth, setDateOfBirth] = useState('')
+
+
+
+
+
+  useEffect(() => {if (!isLoggedIn) redirect(routes.LOGIN);}, [isLoggedIn, path, redirect]);
+
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className='profile-page__wrapper'>
+      <h1 className='profile-page_title'>Profile Page</h1>
+      <fieldset className='customer-data__container'>
+        <legend>Customer Data</legend>
+        <div className='customer-data_item'>
+          <label>First Name:</label>
+          <span>{firstName}</span>
+        </div>
+        <div className='customer-data_item'>
+          <label>Last Name:</label>
+          <span>{lastName}</span>
+        </div>
+        <div className='customer-data_item'>
+          <label>Date Of Birth:</label>
+          <span>{dateOfBirth}</span>
+        </div>
+        <button onClick={() => setModalActive(true)}>Edit profile</button>
+      </fieldset>
     </div>
   );
 };
