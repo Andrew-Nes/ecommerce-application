d4596ece4ca99a8fa880355e9c87b9691e25ec49
Mikita <anykey2solo@gmail.com>
2023-08-27 22:59:28 +0300

    refactor: add classes to elements

--- ecommerce-application/src/components/pages/ProfilePage/ProfilePage.tsx
+++ ecommerce-application/src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,28 +1,41 @@
-import './profile-page.scss';
+import './ProfilePage.scss';
 import { routes } from '../../../types/routingTypes';
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { LogInContext } from '../../app';
 import { FC } from 'react';
 import MyModal from '../../Modal/MyModal';
+import { ClientResponse, Customer } from '@commercetools/platform-sdk';
+import { GetCustomer } from '../../../api/apiFunctions';
 
 const ProfilePage: FC = () => {
   const isLoggedIn = useContext(LogInContext);
   const path = useLocation();
   const redirect = useNavigate();
-  const acessToken = window.localStorage.getItem('token') || ''
+  const accessToken = window.localStorage.getItem('token') || ''
 
   const [modalActive, setModalActive] = useState(false)
+  const [version, setVersion] = useState(0)
 
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [dateOfBirth, setDateOfBirth] = useState('')
 
-
-
-
+  async function setProfile () {
+    try {      
+      const response: ClientResponse<Customer> | undefined = await GetCustomer(accessToken)
+      setVersion(response.body.version)
+      setFirstName(response?.body.firstName || '')
+      setLastName(response?.body.lastName || '')
+      setDateOfBirth(response?.body.dateOfBirth || '')
+    }
+    catch(error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {if (!isLoggedIn) redirect(routes.LOGIN);}, [isLoggedIn, path, redirect]);
+  useEffect(() => {setProfile()},[])
 
   return (
     <div className='profile-page__wrapper'>
@@ -30,18 +43,18 @@ const ProfilePage: FC = () => {
       <fieldset className='customer-data__container'>
         <legend>Customer Data</legend>
         <div className='customer-data_item'>
-          <label>First Name:</label>
-          <span>{firstName}</span>
+          <label className='customer-data-item_label'>First Name:</label>
+          <span className='customer-data-item_content'>{firstName}</span>
         </div>
         <div className='customer-data_item'>
-          <label>Last Name:</label>
-          <span>{lastName}</span>
+          <label className='customer-data-item_label'>Last Name:</label>
+          <span className='customer-data-item_content'>{lastName}</span>
         </div>
         <div className='customer-data_item'>
-          <label>Date Of Birth:</label>
-          <span>{dateOfBirth}</span>
+          <label className='customer-data-item_label'>Date Of Birth:</label>
+          <span className='customer-data-item_content'>{dateOfBirth}</span>
         </div>
-        <button onClick={() => setModalActive(true)}>Edit profile</button>
+        <button className='edit-profile_button' onClick={() => setModalActive(true)}>Edit profile</button>
         <MyModal
           active={modalActive}
           setActive={setModalActive}/>
