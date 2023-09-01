ff204863d44c62a4298b98c9672c3afcf17f7369
Mikita <anykey2solo@gmail.com>
2023-09-02 00:20:10 +0300

    refactor: apply prettier

--- ecommerce-application/src/components/Modal/MyModa.tsx
+++ ecommerce-application/src/components/Modal/MyModa.tsx
@@ -1,18 +1,26 @@
-import './MyModal.scss'
+import './MyModal.scss';
 interface MyModalProps {
-    active: boolean,
-    setActive: React.Dispatch<React.SetStateAction<boolean>>,
-    children?: React.ReactNode | string
+  active: boolean;
+  setActive: React.Dispatch<React.SetStateAction<boolean>>;
+  children?: React.ReactNode | string;
 }
 
-const MyModal = ({active, setActive, children}: MyModalProps) => {
-    return (
-        <div className={active ? 'modal active' : 'modal'} onClick={()=> setActive(false)}>
-            <div className={active ? 'modal__content active' : 'modal__content'} onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => e.stopPropagation()}>
-                {children}
-            </div>
-        </div>
-    )
-}
+const MyModal = ({ active, setActive, children }: MyModalProps) => {
+  return (
+    <div
+      className={active ? 'modal active' : 'modal'}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={active ? 'modal__content active' : 'modal__content'}
+        onClick={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
+          e.stopPropagation()
+        }
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
 
-export default MyModal
+export default MyModal;
