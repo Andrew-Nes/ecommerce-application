558767a719a83febd6afb1fec92d491b3240e099
Mikita <anykey2solo@gmail.com>
2023-09-02 23:09:37 +0300

    feat: creat add address form component

---
+++ ecommerce-application/src/components/Forms/AddAddressForm/AddAddressForm.tsx
@@ -0,0 +1,78 @@
+import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { validateFields } from '../RegistrationForm/validateFields';
+import MyAddressInput from './AddAddressInput/AddAddressInput';
+import {
+  AddAddressFormProps,
+  AddAddressFormData,
+} from '../../../types/profilePageTypes';
+import MyAddressSelectInput from './AddAddressInput/AddAddressSelect';
+
+export const AddAddressForm: FC<AddAddressFormProps> = (
+  props: AddAddressFormProps
+) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<AddAddressFormData>({ mode: 'all' });
+
+  const onSubmit: SubmitHandler<AddAddressFormData> = (
+    data: AddAddressFormData
+  ) => {
+    console.log(data);
+    console.log(props);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <MyAddressInput
+          register={register}
+          errors={errors}
+          name="city"
+          title="City:"
+          validate={validateFields.CITY_VALIDATE}
+        />
+
+        <MyAddressInput
+          register={register}
+          errors={errors}
+          name="streetName"
+          title="Street:"
+          validate={validateFields.STREET_VALIDATE}
+        />
+
+        <MyAddressInput
+          register={register}
+          errors={errors}
+          name="postalCode"
+          title="Postal Code:"
+          validate={validateFields.POSTAL_CODE_VALIDATE}
+        />
+
+        <MyAddressInput
+          register={register}
+          errors={errors}
+          name="state"
+          title="State:"
+          validate={validateFields.CITY_VALIDATE}
+        />
+
+        <MyAddressSelectInput
+          register={register}
+          errors={errors}
+          name="country"
+          title="Country:"
+          countries={['US']}
+        />
+
+        <button type="submit" disabled={!isValid}>
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddAddressForm;
