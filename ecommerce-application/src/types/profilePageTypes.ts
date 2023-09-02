cd0e63fcd6a421592605a5b55682d2fb8148cbd5
Mikita <anykey2solo@gmail.com>
2023-09-02 12:45:08 +0300

    feat: add types for profile card and address

--- ecommerce-application/src/types/profilePageTypes.ts
+++ ecommerce-application/src/types/profilePageTypes.ts
@@ -1,9 +1,23 @@
-import { Customer } from '@commercetools/platform-sdk';
+import { Address, Customer } from '@commercetools/platform-sdk';
 import { Dispatch, SetStateAction } from 'react';
 
-interface ProfileCardProps {
+interface ProfileCardAddressProps {
   currentCustomer: Customer | undefined;
   isUpdateData: Dispatch<SetStateAction<boolean>>;
 }
 
-export type { ProfileCardProps };
+interface AddressItemProps {
+  address: Address;
+  isShipping: boolean;
+  isBilling: boolean;
+  isDefaultShipping: boolean;
+  isDefaultBilling: boolean;
+  key: string;
+  isUpdateData: Dispatch<SetStateAction<boolean>>;
+  version: number | undefined;
+  addressID: string;
+  defaultShippingID: string | undefined;
+  defaultBillingID: string | undefined;
+}
+
+export type { ProfileCardAddressProps, AddressItemProps };
