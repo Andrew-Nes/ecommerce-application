e62c4e8b482ea501a6c2c2d6dbea0ab790df5e8f
Mikita <anykey2solo@gmail.com>
2023-09-02 12:17:55 +0300

    feat: create addresslist component

---
+++ ecommerce-application/src/components/Pages/ProfilePage/AddressList/AddressList.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import { ProfileCardAddressProps } from '../../../../types/profilePageTypes';
+import AddressItem from './AddressItem/AddressItem';
+import './AddressList.scss';
+
+const AddressLists: FC<ProfileCardAddressProps> = (
+  props: ProfileCardAddressProps
+) => {
+  return (
+    <fieldset className="address-list__wrapper">
+      <legend>Addresses</legend>
+      <ul className="address-list__container">
+        {props.currentCustomer?.addresses.map((address, index) => {
+          return (
+            <AddressItem
+              version={props.currentCustomer?.version}
+              isUpdateData={props.isUpdateData}
+              address={address}
+              isShipping={
+                props.currentCustomer?.shippingAddressIds?.includes(
+                  address.id || ''
+                )
+                  ? true
+                  : false
+              }
+              isBilling={
+                props.currentCustomer?.billingAddressIds?.includes(
+                  address.id || ''
+                )
+                  ? true
+                  : false
+              }
+              isDefaultBilling={
+                props.currentCustomer?.defaultBillingAddressId === address.id
+                  ? true
+                  : false
+              }
+              isDefaultShipping={
+                props.currentCustomer?.defaultShippingAddressId === address.id
+                  ? true
+                  : false
+              }
+              key={`'key'${index}`}
+              addressID={address.id || ''}
+              defaultShippingID={
+                props.currentCustomer?.defaultShippingAddressId
+              }
+              defaultBillingID={props.currentCustomer?.defaultBillingAddressId}
+            />
+          );
+        })}
+      </ul>
+    </fieldset>
+  );
+};
+
+export default AddressLists;
