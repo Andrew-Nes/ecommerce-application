b5be3f6fb7c99ea10c5ba7a7dec3578d3100a4e6
Mikita <anykey2solo@gmail.com>
2023-09-02 14:47:00 +0300

    feat: add EditProfile Form type

--- ecommerce-application/src/types/profilePageTypes.ts
+++ ecommerce-application/src/types/profilePageTypes.ts
@@ -1,5 +1,6 @@
 import { Address, Customer } from '@commercetools/platform-sdk';
 import { Dispatch, SetStateAction } from 'react';
+import { UseFormRegister, FieldErrors, Validate } from 'react-hook-form';
 
 interface ProfileCardAddressProps {
   currentCustomer: Customer | undefined;
@@ -20,4 +21,31 @@ interface AddressItemProps {
   defaultBillingID: string | undefined;
 }
 
-export type { ProfileCardAddressProps, AddressItemProps };
+interface EditProfileFormProps {
+  currentCustomer: Customer | undefined;
+  isActive: boolean,
+  isUpdateData: Dispatch<SetStateAction<boolean>>
+  setModalActive: Dispatch<SetStateAction<boolean>>
+}
+
+interface EditProfileFormData {
+  firstName: string,
+  lastName:string,
+  dateOfBirth: string
+  email: string
+}
+type InputEditForm = 'firstName' | 'lastName' | 'email' | 'dateOfBirth'
+interface MyProfileInputProps {
+  register: UseFormRegister<EditProfileFormData>,
+  errors: FieldErrors<EditProfileFormData>;
+  name: InputEditForm;
+  title: string;
+  validate?:
+  | Validate<string, EditProfileFormData>
+  | Record<string, Validate<string, EditProfileFormData>>
+  | undefined;
+  type?: string;
+  value?: string
+}
+
+export type { ProfileCardAddressProps, AddressItemProps, EditProfileFormProps, EditProfileFormData, MyProfileInputProps };
