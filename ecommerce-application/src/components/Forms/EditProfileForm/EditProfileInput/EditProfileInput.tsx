27750551c3555180dce21603be0b19cfa90cc906
Mikita <anykey2solo@gmail.com>
2023-09-02 15:38:07 +0300

    refactor: correct style to EditProfile Form

--- ecommerce-application/src/components/Forms/EditProfileForm/EditProfileInput/EditProfileInput.tsx
+++ ecommerce-application/src/components/Forms/EditProfileForm/EditProfileInput/EditProfileInput.tsx
@@ -1,29 +1,35 @@
 import { FC } from 'react';
 import './EditProfileInput.scss';
 import { MyProfileInputProps } from '../../../../types/profilePageTypes';
+import { errorsMessage } from '../../../../types/formTypes';
 
 const MyProfileInput: FC<MyProfileInputProps> = (
   props: MyProfileInputProps
 ) => {
-  return (
-    <div className="profile-input__container">
-      <label className="profile-input_label">{props.title}:</label>
-      <input
-        className={`profile-input-input ${
-          props.errors[props.name] ? 'input__error' : ''
-        }`}
-        type={props.type}
-        {...props.register(props.name, {
-          required: 'required',
-          validate: props.validate,
-        })}
-        title={props.name}
-      />
-      <span className="error__message" title={`${props.name}Error`}>
-        {props.errors[props.name]?.message}
-      </span>
-    </div>
-  );
+    let requiredErrorText: string;
+    props.type === 'date'
+      ? (requiredErrorText = errorsMessage.DATE_OF_BIRTH_REQUIRED)
+      : (requiredErrorText = `${props.title} is required`);
+    return (
+      <div
+        className="edit-profile-input-wrapper"
+      >
+        <label className="edit-profile-label">{props.title}:</label>
+        <input
+          className={`input edit-profile__input ${
+            props.errors[props.name] ? 'input__error' : ''}`}
+          type={props.type}
+          {...props.register(props.name, {
+            required: requiredErrorText,
+            validate: props.validate,
+          })}
+          title={props.name}
+        />
+        <span className="error__message" title={`${props.name}Error`}>
+          {props.errors[props.name]?.message}
+        </span>
+      </div>
+    );
 };
 
 export default MyProfileInput;
