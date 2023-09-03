5c9676dd586da24d4d877651063919cacc504951
Andrei Niasmachny <Cejosoll@gmail.com>
2023-09-03 17:32:51 +0300

    feat: update slider

--- ecommerce-application/src/components/Slider/Slider.tsx
+++ ecommerce-application/src/components/Slider/Slider.tsx
@@ -1,48 +1,60 @@
 import { FC, useState } from 'react';
 import './slider.scss';
 import { Image } from '@commercetools/platform-sdk';
+import Lightbox, { SlideImage } from 'yet-another-react-lightbox';
+import { Inline } from 'yet-another-react-lightbox/plugins';
+import 'yet-another-react-lightbox/styles.css';
 
 const Slider: FC<{ sliderImages: Image[] | undefined }> = (props) => {
-  const [current, setCurrent] = useState(0);
-  const length = props.sliderImages ? props.sliderImages.length : 0;
-  const imagesArray = props.sliderImages ? props.sliderImages : [];
+  const [open, setOpen] = useState(false);
+  const [index, setIndex] = useState(0);
 
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
+  const toggleOpen = (state: boolean) => () => setOpen(state);
 
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
+  const updateIndex = ({ index: current }: { index: number }) =>
+    setIndex(current);
 
-  if (!Array.isArray(imagesArray) || imagesArray.length <= 0) {
-    return null;
-  }
-
+  const images: SlideImage[] | undefined = props.sliderImages?.map((el) => {
+    return {
+      src: el.url,
+      width: el.dimensions.w,
+      height: el.dimensions.h,
+    };
+  });
   return (
-    <div className="slider">
-      <button className="slider__arrow left" onClick={prevSlide} />
-      <button className="slider__arrow right" onClick={nextSlide} />
-      {imagesArray.map((element) => {
-        return (
-          <div
-            className={
-              imagesArray.indexOf(element) === current
-                ? 'slider__slide active'
-                : 'slider__slide'
-            }
-            key={imagesArray.indexOf(element)}
-          >
-            {imagesArray.indexOf(element) === current && (
-              <img
-                src={imagesArray[current].url}
-                alt="product image"
-                className="product-image"
-              />
-            )}
-          </div>
-        );
-      })}
+    <div className="product-page__slider">
+      <Lightbox
+        index={index}
+        slides={images}
+        plugins={[Inline]}
+        on={{
+          view: updateIndex,
+          click: toggleOpen(true),
+        }}
+        carousel={{
+          padding: 0,
+          spacing: 0,
+          imageFit: 'cover',
+        }}
+        inline={{
+          style: {
+            width: '100%',
+            maxWidth: '800px',
+            aspectRatio: '3 / 2',
+            margin: '0 auto',
+          },
+        }}
+      />
+
+      <Lightbox
+        open={open}
+        close={toggleOpen(false)}
+        index={index}
+        slides={images}
+        on={{ view: updateIndex }}
+        animation={{ fade: 0 }}
+        controller={{ closeOnPullDown: true, closeOnBackdropClick: true }}
+      />
     </div>
   );
 };
