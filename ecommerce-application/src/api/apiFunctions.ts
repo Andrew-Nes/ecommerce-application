f5eaa33ec9d0cdb5d2e011685739e54b059ef92f
Mikita <anykey2solo@gmail.com>
2023-09-04 21:29:43 +0300

    fix: apply prettier

--- ecommerce-application/src/api/apiFunctions.ts
+++ ecommerce-application/src/api/apiFunctions.ts
@@ -105,9 +105,7 @@ export const GetCustomer = async () => {
   return await client.me().get().execute();
 };
 
-export const UpdateCustomer = async (
-  updateCustomer: MyCustomerUpdate
-) => {
+export const UpdateCustomer = async (updateCustomer: MyCustomerUpdate) => {
   const client = getCurrentClient();
   return await client
     .me()
